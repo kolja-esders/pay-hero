d3afc41568e8c061db540d0f39ff85fa8e16821b
Kolja Esders <kolja.esders@gmail.com>
2016-11-26 19:31:57 +0100

    Added payment success activity

---
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/PaymentSuccessActivity.java
@@ -0,0 +1,13 @@
+package edu.pietro.team.payhero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_success);
+    }
+}
