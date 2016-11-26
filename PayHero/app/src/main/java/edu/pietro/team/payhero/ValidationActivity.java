24e213588832adf5449c34e8e37c203195626a4e
Kolja Esders <kolja.esders@gmail.com>
2016-11-26 23:35:12 +0100

    Validation overhauled

--- PayHero/app/src/main/java/edu/pietro/team/payhero/ValidationActivity.java
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/ValidationActivity.java
@@ -6,8 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import edu.pietro.team.payhero.helper.PostHelper;
 
 public class ValidationActivity extends AppCompatActivity {
 
@@ -17,6 +22,21 @@ public class ValidationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_validation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button okay = (Button) findViewById(R.id.button);
+        okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = ((EditText)findViewById(R.id.nameEdit)).getText().toString();
+                String iban = ((TextView)findViewById(R.id.ibanEdit)).getText().toString();
+                String amount = ((TextView)findViewById(R.id.amountEdit)).getText().toString();
+                try {
+                    PostHelper.transfer("DE48201100223000060898", "XXX", "5.00");
+                } catch (Exception e) {
+                    Log.e("PAYMENT", "Payment failed :/", e);
+                }
+
+            }
+        });
     }
 
     @Override
@@ -27,21 +47,17 @@ public class ValidationActivity extends AppCompatActivity {
         boolean hasIban = intent.hasExtra("iban");
         boolean hasName = intent.hasExtra("name");
         if (hasIban && hasName) {
-            findViewById(R.id.ibanTextEdit).setVisibility(View.INVISIBLE);
-            ((TextView)findViewById(R.id.nameTextView)).setText(intent.getStringExtra("name"));
-            ((TextView)findViewById(R.id.ibanTextView)).setText("( " + intent.getStringExtra("iban") + " )");
+            ((EditText)findViewById(R.id.nameEdit)).setText(intent.getStringExtra("name"));
+            ((EditText)findViewById(R.id.ibanEdit)).setText(intent.getStringExtra("iban"));
         } else if (hasName) {
-            ((TextView)findViewById(R.id.nameTextView)).setText(intent.getStringExtra("name"));
-            findViewById(R.id.ibanTextEdit).setVisibility(View.INVISIBLE);
-            findViewById(R.id.ibanTextView).setVisibility(View.INVISIBLE);
+            ((TextView)findViewById(R.id.nameEdit)).setText(intent.getStringExtra("name"));
         } else /* hasIban == true !! */ {
-            ((TextView)findViewById(R.id.ibanTextView)).setText("( " + intent.getStringExtra("iban") + " )");
-            findViewById(R.id.ibanTextEdit).setVisibility(View.INVISIBLE);
-            findViewById(R.id.nameTextView).setVisibility(View.INVISIBLE);
+            ((TextView)findViewById(R.id.ibanEdit)).setText(intent.getStringExtra("iban"));
+            findViewById(R.id.ibanEdit).setVisibility(View.VISIBLE);
         }
 
         String amountStr = String.format("%1$.2f", intent.getDoubleExtra("amount", 0.0)).replace('.',',') + " €";
-        ((TextView)findViewById(R.id.amountTextView)).setText(amountStr);
+        ((TextView)findViewById(R.id.amountEdit)).setText(amountStr);
 
         if (intent.hasExtra("reason")) {
             ((TextView)findViewById(R.id.reasonTextEdit)).setText(intent.getStringExtra("reason"));
