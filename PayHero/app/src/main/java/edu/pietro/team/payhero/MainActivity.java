af1164c380ea3ab09f543388aef9ed4bc9d56082
Kolja Esders <kolja.esders@gmail.com>
2016-11-26 12:29:01 +0100

    Created activity and switching to it in the beginning.

--- PayHero/app/src/main/java/edu/pietro/team/payhero/MainActivity.java
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.pietro.team.payhero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
     }
 }
---
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/ScanActivity.java
@@ -0,0 +1,31 @@
+package edu.pietro.team.payhero;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.pietro.team.payhero.R;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
