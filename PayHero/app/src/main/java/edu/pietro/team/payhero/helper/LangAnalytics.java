d8b8de1c7cbf735a40f54c99398c9bfc668c2bdb
dzimmerer <dzimmerer@gmail.com>
2016-11-26 15:54:30 +0100

    The Euro is back (except in the UK)

--- PayHero/app/src/main/java/edu/pietro/team/payhero/helper/LangAnalytics.java
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/helper/LangAnalytics.java
@@ -15,9 +15,19 @@ import org.apache.commons.lang3.*;
 public class LangAnalytics {
 
 
-    static final String regExp = "[0-9]+([,.][0-9]{1,2})[€]*?";
-    static final Pattern pattern = Pattern.compile(regExp);
-    
+    static final String regExp1 = "[0-9]+([,.][0-9]{2})[€]*?";
+    static final Pattern pattern1 = Pattern.compile(regExp1);
+
+    static final String regExp2 = "[0-9]+(([,.][0-9][0-9]){0,1})[€]+?";
+    static final Pattern pattern2 = Pattern.compile(regExp2);
+
+
+    public static void main(String args[]){
+
+        System.out.println(getAmount("4.00 5€"));
+
+    }
+
 
     public static String getIBAN(String text){
 
@@ -41,7 +51,17 @@ public class LangAnalytics {
 
         for(int i = 0; i < splitText.length ; i++){
             String subText = splitText[i];
-            Matcher matcher = pattern.matcher(subText);
+            Matcher matcher = pattern2.matcher(subText);
+            if(matcher.matches()){
+                subText = subText.replace(",",".");
+                subText = subText.replace("€","");
+                return subText;
+            }
+        }
+
+        for(int i = 0; i < splitText.length ; i++){
+            String subText = splitText[i];
+            Matcher matcher = pattern1.matcher(subText);
             if(matcher.matches()){
                 subText = subText.replace(",",".");
                 subText = subText.replace("€","");
