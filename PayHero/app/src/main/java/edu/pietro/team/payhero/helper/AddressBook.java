9fafb82ede5f81b1b1daceda1d4d1af87b16f0a1
maxim <maxim.koehler@gmx.de>
2016-11-27 13:16:03 +0100

    Polizei Stuttgart added

--- PayHero/app/src/main/java/edu/pietro/team/payhero/helper/AddressBook.java
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/helper/AddressBook.java
@@ -54,7 +54,8 @@ public class AddressBook {
     public static final Contact[] CONTACTS = {
             new Contact("Kolja Esders", "DE81100100100005578146", "2f154220-39bf-4b64-8b36-b2cce3a68492", R.drawable.kolja),
             new Contact("Maxim Köhler", "DE81100100100005578146", "829e847e-29d4-45aa-aba2-4704981d27f1", R.drawable.maxim),
-            new Contact("David Zimmerer", "DE81100100100005578146", "0704a7f3-dda2-4001-b064-e082f9ee036c", R.drawable.david)
+            new Contact("David Zimmerer", "DE81100100100005578146", "0704a7f3-dda2-4001-b064-e082f9ee036c", R.drawable.david),
+            new Contact("Polizei Stuttgart", "DE81100100100005578146", "aslkdklsjda", -1)
     };
 
 
