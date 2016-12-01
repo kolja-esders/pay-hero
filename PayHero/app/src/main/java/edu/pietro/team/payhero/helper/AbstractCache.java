7e110a6d330ae0adb5518e5f2e6c745dfab0621b
Kolja Esders <kolja.esders@gmail.com>
2016-12-01 03:05:44 +0100

    AbstractCache improvements

---
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/helper/AbstractCache.java
@@ -0,0 +1,33 @@
+package edu.pietro.team.payhero.helper;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+
+public abstract class AbstractCache<T> {
+
+    private String mPrefix;
+
+    public AbstractCache() {
+        mPrefix = "";
+    }
+
+    public AbstractCache(String prefix) {
+        if (null == prefix) {
+            throw new NullPointerException("prefix == null");
+        }
+        mPrefix = prefix;
+    }
+
+    // Later on...
+    // void put(String key, T value, Calendar expirationDate);
+
+    public abstract void put(String key, T value) throws IOException;
+
+    public abstract T get(String key) throws IOException;
+
+    protected String constructFullKey(String key) {
+        return mPrefix.isEmpty() ? key : StringUtils.join(mPrefix, "\\", key);
+    }
+}
--- PayHero/app/src/main/java/edu/pietro/team/payhero/helper/GenericCallbackInterface.java
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/helper/GenericCallbackInterface.java
@@ -4,7 +4,7 @@ package edu.pietro.team.payhero.helper;
  * Since there is apparently no proper generic callback <3.
  * @param <T>
  */
-public interface GenericCallback<T> {
+public interface GenericCallbackInterface<T> {
 
     void onSuccess(T result);
 
