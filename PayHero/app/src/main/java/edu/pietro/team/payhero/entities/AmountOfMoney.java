322c86c6bd9dfe6a6dd87f74dd53dab5b21a062e
Kolja Esders <kolja.esders@gmail.com>
2016-12-20 23:40:18 +0100

    Some money related stuff.

--- PayHero/app/src/main/java/edu/pietro/team/payhero/entities/AmountOfMoney.java
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/entities/AmountOfMoney.java
@@ -9,4 +9,27 @@ public class AmountOfMoney {
 
     private Currency mCurrency;
 
+    public AmountOfMoney() {
+    }
+
+    public AmountOfMoney(Double amount, Currency currency) {
+        mAmount = amount;
+        mCurrency = currency;
+    }
+
+    public Double getAmount() {
+        return mAmount;
+    }
+
+    public void setAmount(Double amount) {
+        this.mAmount = amount;
+    }
+
+    public Currency getCurrency() {
+        return mCurrency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.mCurrency = currency;
+    }
 }
