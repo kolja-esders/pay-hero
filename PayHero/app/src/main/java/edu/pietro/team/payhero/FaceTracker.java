fb329748e7acf5ed18b3ac5ccaf2243b1c070e26
Kolja Esders <kolja.esders@gmail.com>
2016-12-25 04:46:45 +0100

    New preview surface + face detection

---
+++ PayHero/app/src/main/java/edu/pietro/team/payhero/FaceTracker.java
@@ -0,0 +1,27 @@
+package edu.pietro.team.payhero;
+
+
+import android.util.Log;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+
+public class FaceTracker extends Tracker<Face> {
+
+    private final static String TAG = "FaceTracker";
+
+    public void onNewItem(int id, Face face) {
+        Log.i(TAG, "Face detected :o");
+    }
+
+    public void onUpdate(Detector.Detections<Face> detections, Face face) {
+        if (face.getIsSmilingProbability() > 0.75) {
+            Log.i(TAG, "Keep smiling!");
+        }
+    }
+
+    public void onDone() {
+        Log.i(TAG, "Don't leave!");
+    }
+}
